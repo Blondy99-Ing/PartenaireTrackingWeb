53269868b10ca7def57be720cffa703fe4ec230b
Proxym <it_management@proxymgroup.com>
2026-06-01 12:46:46 +0100

    redeploiement pour la creation des coupure message d'erreur

--- app/Http/Controllers/Gps/ControlGpsController.php
+++ app/Http/Controllers/Gps/ControlGpsController.php
@@ -12,6 +12,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
+use App\Support\UserMessages;
 
 class ControlGpsController extends Controller
 {
